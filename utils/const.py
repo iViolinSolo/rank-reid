f111b558a09b73848db3a2c13adbc039be028da0
ViolinSolo <i.violinsolo@gmail.com>
2018-11-05 00:26:12 +0800

    init

---
+++ utils/const.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: violinsolo
+# Created on 05/11/2018
+
+DATASET_ROOT_PATH = '/Users/violinsolo/dev/02 Dataset/person re-id/Market-1501-v15.09.15/_rerank'
+PROJECT_ROOT_PATH = '/Users/violinsolo/GitHub/rank-reid'
